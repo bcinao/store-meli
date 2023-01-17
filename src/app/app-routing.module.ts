84ce2026864fe6ee04dcea6b47bdc880521d97da
Baldassare Cinao <bcinao@queplan.cl>
2023-01-17 11:47:33 -0300

    CHG Se agregan las rutas lazy loading de todas las paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'items',
+    loadChildren: () =>
+      import('./pages/results/results.module').then((m) => m.ResultsModule),
+  },
+  {
+    path: 'items/:id',
+    loadChildren: () =>
+      import('./pages/detail/detail.module').then((m) => m.DetailModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
